9008defec51eaa5b7e401bd3053c804c9c670330
NasK1991 <alex_nask91@hotmail.com>
2016-04-16 13:42:59 +0200

    el evento de la celda se manda al socket

--- Assets/IsoUnity/Source/EvenManagers/EventMark.cs
+++ Assets/IsoUnity/Source/EvenManagers/EventMark.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class EventMark : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () { Debug.Log("Inicializada la collision"); }
+	void Start () {}
 	
 	// Update is called once per frame
 	void Update () {}
 
     void OnCollisionEnter(Collision collisionInfo) {
         string json = "{\"name\":\"event\",\"parameters\":{\"eventName\":\"tocar campana\"}}";
-        Connection.getInstance().sendEvent(false, json);
+        Connection.getInstance().sendEvent(true, json);
         //Debug.Log("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
         //Debug.Log("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
         //Debug.Log("Their relative velocity is " + collisionInfo.relativeVelocity);
